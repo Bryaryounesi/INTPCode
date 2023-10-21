9f9914d66d38edd52d374aa9c588ce2c028b39e5
B-Younesi <narmabr@gmail.com>
2023-10-21 10:24:07 +0330

    first week python exercise

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
-print("salalm")
-print("1+1")
+print("hello world")
+first_name= "Elizabeth"
+last_name = "tailor"
+name = "Elizabeth tailor"
+ticket = 200
+ticket2 = ticket * 10
+city = "las vegas"
+
+print (ticket2)
+
+print("have a delicious time")
 print("bye")
-print("1+1")
