190e7d21ae7cceed91643172687d27c1c5ebda85
B-Younesi <narmabr@gmail.com>
2023-11-01 15:11:26 +0330

    Create second week python training.py

---
+++ second week python training.py
@@ -0,0 +1,64 @@
+print("hello world")
+name = "hasan"
+print(type(name))
+number = 35
+print(type(number))
+sum = 18.26
+print(type(sum))
+alive  = True
+print(type(alive))
+
+age = "18"
+print(type(age))
+age_convered = int(age)
+print(age_convered)
+print(type(age_convered))
+print(age_convered <= 18)
+
+price = 12.3
+price_converted = int(price)
+print(price_converted)
+print(int(price))
+
+week = 12
+print(float(week))
+
+member = True
+not_member = False
+value = int(member)
+second_value = int(not_member)
+print(value)
+print(second_value)
+print(int(member))
+print(int(not_member))
+
+member_name = "sam"
+middle_name = "" 
+sibling = 0 
+foot_size = 8.5
+print(bool(member_name))
+print(bool(sibling))
+print(bool(foot_size))
+print(bool(middle_name))
+
+detail = "i love you"
+response = bool(detail)
+print(response)
+
+print(f"{12} new masage")
+print("comment_number:" + "12")
+print(12+5)
+print(f"{13} comments")
+print(f"{23} new message")
+print(f"{4+3} new message and {50} comments")
+apple = 40
+orange = 36
+print(f"{apple*orange} new message and {24} comments")
+
+new = 51
+status = f"{new} new massage"
+print(status)
+
+print(f"I would walk  {500}  miles")
+price = 40
+print(f"glasses price: {price} dollar")
