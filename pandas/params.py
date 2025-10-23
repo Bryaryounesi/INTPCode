ab5b65cf5fd723b2f0146f5cc9e75175ee066783
Bryar Younesi <147278172+Bryaryounesi@users.noreply.github.com>
2025-10-23 12:17:36 +0330

    Update params.py

--- pandas/params.py
+++ pandas/params.py
@@ -60,7 +60,7 @@ with pd.ExcelWriter(expath,mode='a',if_sheet_exists= "new") as writer:
 expath
 mode = "a"
 if_sheet_exists= "new"بهتر /"replace"/"error"
-----------------------------------------------------------
+-----------------------------------------------
 pd.concat
 شکل رایج
 dfall = pd.concat([df1,df2,...],ignore_index=True)
@@ -68,4 +68,22 @@ dfall = pd.concat([df1,df2,...],ignore_index=True)
 objs = [,] / (,)
 ignore_index = False بهتر/ پیشفرض True 
 axis = پیش فرض 0 / 1
+
+-----------------------------------------------
+pd.merge
+شکل رایج 
+
+df = pd.merge(L,R, how='inner', on='col_name')
+left	 = سمت چپ df
+right	 =  سمت راست df 
+
+how	 = نوع join ('inner', 'left', 'right', 'outer')
+
+on	 = نام ستون مشترک بین دو df
+
+left_on	= 
+اگر نام ستون در df سمت چپ متفاوت باشد
+
+right_on	= 
+اگر نام ستون در df  سمت راست متفاوت باشد
 '''
